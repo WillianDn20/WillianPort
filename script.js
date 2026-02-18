72c5fbf7b8e54d5c3e824a2e17976dffb04316da
Willian Dias <WillianDias442@icloud.com>
2026-02-17 21:13:46 -0300

    BkHtml

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ function toggleMode() {
 
     body.classList.toggle("light")
 
+    updateThemeColor()
+
+}
+
+function updateThemeColor() {
+  const meta = document.querySelector('meta[name="theme-color"]')
+  const isLight = document.body.classList.contains("light")
+
+  if (isLight) {
+    meta.setAttribute("content", "#ffffff")
+  } else {
+    meta.setAttribute("content", "#000000")
+  }
 }
